c67a043590cac38d06e5aae587413f73dacb8605
Markus Ahlstrand <markus@ahlstrand.es>
2025-10-04 16:44:22 +0200

    feat: update auth middleware to handle permissions

--- dts-bundle-generator.config.ts
+++ dts-bundle-generator.config.ts
@@ -3,9 +3,15 @@ const config = {
     {
       filePath: './src/index.ts',
       outFile: './build/hono-openapi-middlewares.d.ts',
-      noCheck: false,
+      noCheck: true, // Skip checking due to issues in Hono's type definitions
+      libraries: {
+        inlinedLibraries: [],
+      },
     },
   ],
+  compilationOptions: {
+    preferredConfigPath: './tsconfig.dts.json',
+  },
 };
 
 module.exports = config;
